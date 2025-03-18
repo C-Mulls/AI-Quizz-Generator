736e0be663ed0451dc86431aa334f63559a09232
C-Mulls <connormullins96@yahoo.com>
2025-03-18 19:17:45 -0400

    new url in stripe route.ts to new base

--- src/app/api/stripe/create-portal/route.ts
+++ src/app/api/stripe/create-portal/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
     customer = { id: response.id };
   }
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://ai-quizz-generator.vercel.app/";
 
   const url = await stripe.billingPortal.sessions.create({
     customer: customer.id,
